ab93b41f3362a7dc9456de3ef9b4813b09c2927b
unknown <alex.rocha@dtidigital.com.br>
2021-06-02 11:20:01 -0300

    inserindo nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import DropdownButton from 'react-bootstrap/DropdownButton'
+//import { Dropdown } from 'bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown'
 
 export default function Nav() {
 
@@ -11,7 +14,9 @@ export default function Nav() {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <Link className="nav-link" activeClassName='active' to="/alunos" >Alunos</Link>
@@ -19,22 +24,19 @@ export default function Nav() {
                         <li class="nav-item">
                             <Link className="nav-link" activeClassName='is-active' to="/professores">Professores</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link className="nav-link" activeClassName='is-active' to="/perfil">Perfil</Link>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-          </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" >Action</a></li>
-                                <li><a class="dropdown-item" >Another action</a></li>
-                                <li><a class="dropdown-item" >Something else here</a></li>
-                            </ul>
-                        </li>
+                        
                     </ul>
+
+                    <ul class="navbar-nav">
+                        <DropdownButton menuAlign="left" variant="secondary" id="dropdown-basic-button" title="Dropdown button">
+                            <Dropdown.Item href="/perfil">Perfil</Dropdown.Item>
+                            <Dropdown.Item href="/dashboard">Sair</Dropdown.Item>
+                        </DropdownButton>
+
+                    </ul>
+
                 </div>
             </div>
-        </nav>
+        </nav >
     )
 }
